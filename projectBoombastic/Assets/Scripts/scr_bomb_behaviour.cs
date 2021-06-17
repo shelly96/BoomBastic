c38312c2cd931a5349d08ca1ada5da33c9a33354
shelly96 <65896412+shelly96@users.noreply.github.com>
2021-06-17 11:54:51 +0200

    #3 add bomb timer

--- projectBoombastic/Assets/Scripts/scr_bomb_behaviour.cs
+++ projectBoombastic/Assets/Scripts/scr_bomb_behaviour.cs
@@ -6,7 +6,9 @@ public class scr_bomb_behaviour : MonoBehaviour
 {
     [SerializeField] private float speed = 1.0f;
     [SerializeField] private float shotAngle = 5.0f;
+    [SerializeField] private float bombTimer = 10.0f;
     private Vector2 screenBounds;
+    private float startTime = 0.0f;
 
 
     // Start is called before the first frame update
@@ -19,10 +21,32 @@ public class scr_bomb_behaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // destroy if bomb hit the waves
-        if(-transform.position.y > screenBounds.y + 3)
-        {
+        if(startTime != 0.0f){
+            checkBombExplosion((startTime+bombTimer));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.name);
+
+        switch (collision.collider.name) {
+            case "Wave_2":
+                Destroy(this.gameObject);
+                // TODO add sound 
+                break;
+            case "Boat":
+                startTime = Time.time;
+                break;
+        }
+    }
+
+    private void checkBombExplosion(float explosionTime){
+
+       if(Time.time >= explosionTime) {
             Destroy(this.gameObject);
+            Debug.Log("EXPLODE!");
+            // TODO add sound
         }
     }
 }
