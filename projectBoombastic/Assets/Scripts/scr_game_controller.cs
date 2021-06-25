126f9ac463ed1dfb34c9ddfb017933e8e4fc5a00
Simon <simon.herbig@campus.lmu.de>
2021-06-26 01:20:22 +0200

    #8 game gui
    + added score
    + added hearts prefabs
    + implemented some code

--- projectBoombastic/Assets/Scripts/scr_game_controller.cs
+++ projectBoombastic/Assets/Scripts/scr_game_controller.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class scr_game_controller : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> heartsPrefabs;
+    private float x_pos = -5;
+    private float y_pos = 5;
+
     // Start is called before the first frame update
     void Start()
     {
+        //init hearts/ lives
+        for(int i = 0; i < 3; i++)
+        {
+            Vector2 spawnPos = new Vector2(x_pos, y_pos);
+            GameObject heart = Instantiate<GameObject>(heartsPrefabs[0]);
+            heart.transform.position = spawnPos;
+
+            //set new heart position
+            x_pos += 2;
+        }
         
     }
 
