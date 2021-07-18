cd425450b453fb058dbf28c29eaf37078ef45415
shelly96 <65896412+shelly96@users.noreply.github.com>
2021-07-18 11:35:58 +0200

    #41 clear bomb behaviour

--- projectBoombastic/Assets/Scripts/scr_bomb_behaviour.cs
+++ projectBoombastic/Assets/Scripts/scr_bomb_behaviour.cs
@@ -17,9 +17,8 @@ public class scr_bomb_behaviour : MonoBehaviour
     private bool firstContact = true;
     private bool pickedUp = false;
     private GameObject player;
-
-    // Animation
     private Animator animator;
+
 
     // Start is called before the first frame update
     void Start()
@@ -38,45 +37,44 @@ public class scr_bomb_behaviour : MonoBehaviour
     void Update()
     {
 
-        //picked up 
+        // picked up 
         if (pickedUp) {
-            Debug.Log("Picked up!");
+            //Debug.Log("Picked up!");
             transform.position = new Vector2(player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y + 0.8f);
         }
 
-        //bomb animation
-        if (Time.time >= explosionTime - bombTimer* 0.5f)
-        {
+        // bomb blinking animation 
+        if (Time.time >= explosionTime - bombTimer * 0.5f) {
             animator.SetBool("exploding", true);
         }
 
-        //bomb explosion
+        // bomb explosion
         if (Time.time >= explosionTime) {
             Explode();
         }
 
-        //stop bomb on the boat
+        // stop bomb on the boat
         if(hitBoat) {
             GetComponent<Rigidbody2D>().drag = 1.5f;
         }
 
-        //destroy bombs that miss the water
+        // destroy bombs under the sceene
         if(-transform.position.y > screenBounds.y) {
-            //play sound
+            // play sound if the boat isn't sinking
             if (!GameObject.Find("Boat").GetComponent<scr_boat>().isSinking)
                 GameObject.Find("AudioController").GetComponent<scr_audioController>().playSound("water");
             Destroy(this.gameObject);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnCollisionEnter2D(Collision2D collision) {
 
+        // get first word from a name
         string first_collision_word = collision.collider.name.Split('_')[0];
 
         if ((first_collision_word == "Boat" || first_collision_word == "box" || first_collision_word == "treasure") && pickedUp == false ) {
             hitBoat = true;
-            // play sound first time
+            // play sound for the first time
             if (firstContact ) {
                 GameObject.Find("AudioController").GetComponent<scr_audioController>().playSound("wood");
                 firstContact = false;
@@ -86,65 +84,65 @@ public class scr_bomb_behaviour : MonoBehaviour
         
     }
 
-    private void Explode(){
-        //play sound
+    private void Explode() {
+        
+        // play explosion sound
         GameObject.Find("AudioController").GetComponent<scr_audioController>().playSound("explosion");
 
         // show explosion effect 
         GameObject ExplosionEffect = Instantiate(explosionEffect, transform.position, Quaternion.identity);
         Destroy(ExplosionEffect, 1.0f);
 
-        //destroy bomb object
+        // destroy bomb object
         Destroy(this.gameObject);
-        Debug.Log("BOOM!");
-        //TODO add sound  
+        //Debug.Log("BOOM!");
         
         // get nearby objects
         Collider2D[] bombColliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
             // add force and damage
-            foreach(Collider2D nearbyObject in bombColliders) {
+            foreach (Collider2D nearbyObject in bombColliders) {
                 
-                Debug.Log("Bomb hit: " + nearbyObject.name);
+                //Debug.Log("Bomb hit: " + nearbyObject.name);
                 
-                //force direction for objects
+                // force direction for objects
                 Vector2 direction = nearbyObject.transform.position - transform.position;
 
+                // get first word from a name
                 string first_word = nearbyObject.name.Split('_')[0];
                 
+                // add force or damage to the objects 
                 switch (first_word) {
                     
                     case "Player":
                         nearbyObject.GetComponent<scr_player>().takeDamage();
                         nearbyObject.GetComponent<Rigidbody2D>().AddForce(direction.normalized * explosionPower);
-                        Debug.Log("Player got some damage!");
+                        //Debug.Log("Player got some damage!");
                         break;
                     case "Boat":
                         nearbyObject.GetComponent<scr_boat>().takeDamage();
-                        Debug.Log("Boat got some damage!");
-
+                        //Debug.Log("Boat got some damage!");
                         break;
                     case "box":
-                        //add force, but boat has no Rigidbody so you get an error
                         nearbyObject.GetComponent<Rigidbody2D>().AddForce(direction * explosionPower);
                         break;
                     case "treasure":
-                        //add force, but boat has no Rigidbody so you get an error
                         nearbyObject.GetComponent<Rigidbody2D>().AddForce(direction * explosionPower);
                         break;
                     case "bomb":
                         //destroy other bomb
-                        Debug.Log("BoomBoomBoom");
                         nearbyObject.GetComponent<scr_bomb_behaviour>().reduceExplosionTime(this.explosionTime);
                         break;
                 }
             }
+
     }
 
+    // reduce explosionTime of hit bombs
     private void reduceExplosionTime (float newTime) {
         explosionTime = newTime + explosionDelay;
     }
 
-    // show the Radius in the Bomb Prefab
+    // show the radius in the Bomb Prefab
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, explosionRadius);
